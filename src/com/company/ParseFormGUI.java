2c88c902ce133e2bb1fb5e03a73e4dbd6d3a4adb
Dmitriy Antonov <79038945169@yandex.ru>
2022-03-28 17:51:41 +0300

    2

--- src/com/company/ParseFormGUI.java
+++ src/com/company/ParseFormGUI.java
@@ -1,4 +1,49 @@
 package com.company;
 
-public class ParseFormGUI {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class ParseFormGUI extends JFrame {
+    private JPanel rootPanel;
+    private JButton reButton;
+    private JButton parseButton;
+    private JTextArea textArea;
+
+    public ParseFormGUI() {
+        setContentPane(rootPanel);
+        setLocation(200,200);
+        pack();
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+        parseButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    ArrayList<Post> posts = ConnectAndParse.connectAndParse();
+                    textArea.setColumns(1);
+                    for (Post post : posts){
+                        textArea.append(
+                                post.getAuthor() + " " +
+                                post.getTitle() + " " +
+                                post.getDateOfCreated() + "\n" +
+                                "--" + post.getDetailsLink() + "\n" +
+                                "---" + post.getAuthorDetailsLink() + "\n\n");
+
+                    }
+
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+        reButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 }
